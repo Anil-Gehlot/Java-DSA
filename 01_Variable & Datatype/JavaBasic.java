fa1dd50a8c1b107b18a7e79fa7d19050eac44b48
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-11 16:59:10 +0530

    First program in java

---
+++ 01_Variable & Datatype/JavaBasic.java
@@ -0,0 +1,7 @@
+public class JavaBasic{
+    public static void main(String[] args) {
+        System.out.println("Jai Shree Radhe .....");
+        System.out.print("Jai Shree Radhe .....\n");
+        System.out.println("Jai Shree Radhe .....");
+    }
+}
