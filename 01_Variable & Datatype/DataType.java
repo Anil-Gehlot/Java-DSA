ec65b352091d45297c5c6e2f10e4ad8dc397a325
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-11 16:58:07 +0530

    DataTypes in Java

---
+++ 01_Variable & Datatype/DataType.java
@@ -0,0 +1,52 @@
+
+	public class DataType{
+		public static void main(String args[]){
+			System.out.print("1st Output");
+			System.out.println("2nd Output");
+
+
+
+			// byte takes 1 byte and its range is between -128 to 127.
+			byte b = 121;
+			System.out.println(b);
+
+			// short takes 2 byte and its range is between -32768to 32767.
+			short a = 3424;
+			System.out.println(a);
+
+			// char is used to represent character of ASCII value. it range is between 0 to 255.
+			char c = 65;
+			System.out.println(c);
+
+			char d = 'g';
+			System.out.println(d);
+
+
+			// boolean takes 1 bit and it's value can only be onlt true & false.
+			boolean t = true;
+			System.out.println(t);
+
+			boolean f = false;
+			System.out.println(f);
+
+
+			// int takes 4 bytes. range between -2B se 2B.
+			int e = 234;
+			System.out.println(e);
+
+
+			// long takes 8 byte.
+			long l = 423423;
+			System.out.println(l);
+
+			//  float takes only upto 7 digit of decimal, or har float value ke end me 'f' lagana compulsory he.
+			float m = 1.2435353443464f;
+			System.out.println(m);
+
+			//  double takes only upto 16 digit of decimal,
+			double v = 2.234233342234;
+			System.out.println(v);
+
+
+		}
+	}	  		 	
