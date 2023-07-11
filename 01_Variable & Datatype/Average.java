8571bb4290d2bf13dcbc7d8fb36c6729f918cd83
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-11 16:56:25 +0530

    Finding Average

---
+++ 01_Variable & Datatype/Average.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Average{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter first number: ");
+		int num1 = sc.nextInt();
+
+		System.out.print("Enter second number: ");
+		int num2 = sc.nextInt();
+
+		System.out.print("Enter third number: ");
+		int num3 = sc.nextInt();
+
+		int average = (num1 + num2 + num3)/3;
+		System.out.println("Average of numbers: "+average);
+	}
+}
