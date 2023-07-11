0a6e46fc2d034a7ee49a679f134309fef28b450e
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-11 17:00:35 +0530

    Sum of numbers

---
+++ 01_Variable & Datatype/Sum.java
@@ -0,0 +1,15 @@
+public class Sum{
+	public static void main(String args[]){
+
+		int a = 10;
+		int b = 5;
+		float f = 4.5f;
+
+
+		int sum = a+b;
+		float floatsum = a+f;
+
+		System.out.println("The sum is: "+sum);
+		System.out.println("The sum is: "+floatsum +" "+ sum);
+	}
+}
