b1222a5d5a3a018ee8502ffc17e443833fe32c8e
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-12 18:31:36 +0530

    =, +=, -=, *=, /=, %=

---
+++ 02_operators/_04AssignmentOperator.java
@@ -0,0 +1,36 @@
+
+
+public class _04AssignmentOperator{
+	public static void main(String args[]){
+
+		int a = 5;
+		int b = 4;
+		int c = 3;
+		int d = 2;
+		int e = 1;
+
+
+		// a = a + 1;
+		a +=1;
+
+		// b = b-1;
+		b -= 1;
+
+		// c = c * 5;
+		c *= 5;
+
+		// d = d/2;
+		d /=2;
+
+		// e = e%2;
+		e %= 2;
+
+		System.out.println(a);		// 6
+		System.out.println(b);		// 3
+		System.out.println(c);		// 15
+		System.out.println(d);		// 1
+		System.out.println(e);		// 1
+
+
+	}
+}
