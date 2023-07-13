e4da5d8bcaf3470d5e7930f811279c67ee4347e5
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-13 11:44:35 +0530

    How to use switch statement in java

---
+++ 03_conditional satatement/_08Switch.java
@@ -0,0 +1,18 @@
+
+
+public class _08Switch{
+	public static void main(String args[]){
+
+
+		int number = 2;
+
+		switch(number) {
+
+		case 1 : System.out.println("your given number is 1:");	break;
+		case 2 : System.out.println("your given number is 2:"); break;
+		case 3 : System.out.println("your given number is 3:"); break;
+		case 4 : System.out.println("your given number is 4:"); break;
+		default: System.out.println("Default number.");
+		}
+	}
+}
