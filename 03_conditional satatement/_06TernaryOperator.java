76932334a40680673f36ebafa860af6e277d2196
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-13 11:43:27 +0530

    How to use Ternary operator in java

---
+++ 03_conditional satatement/_06TernaryOperator.java
@@ -0,0 +1,14 @@
+
+
+
+public class _06TernaryOperator{
+	public static void main(String args[]){
+
+
+		int num1 = 5;
+
+
+		String ans = (num1%2==0) ? "Even" : "Odd";
+		System.out.println(ans);
+	}
+}
