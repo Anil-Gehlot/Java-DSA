316874c284f3b2747eab73158aa6a0e2101ea211
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-14 15:22:43 +0530

    How many number of time while loop will run?

---
+++ 04_Loops(Flow control)/_02Number.java
@@ -0,0 +1,15 @@
+
+
+// Printing Number from 1 to 10.
+
+public class _02Number{
+	public static void main(String args[]){
+
+		int num = 1;
+
+		while (num<=10){
+			System.out.println(num);
+			num++;
+		}
+	}
+}
