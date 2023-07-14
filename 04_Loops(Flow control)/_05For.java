a5852d2c612b718de8ab482ad29836aa77ff695d
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-14 15:26:39 +0530

    for loop in java

---
+++ 04_Loops(Flow control)/_05For.java
@@ -0,0 +1,11 @@
+
+// printing hello world 10 times by for loop.
+
+public class _05For{
+	public static void main(String args[]){
+
+		for(int i = 1; i<=10; i++){
+			System.out.println(i + " Hello world...");
+		}
+	}
+}
