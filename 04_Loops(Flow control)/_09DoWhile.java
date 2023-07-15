ed712de19cba33199c79f945df60b88aebb312e4
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-15 11:54:00 +0530

    How to use do-while loop

---
+++ 04_Loops(Flow control)/_09DoWhile.java
@@ -0,0 +1,17 @@
+
+
+
+public class _09DoWhile{
+	public static void main(String args[]){
+
+		int num = 10;
+
+		int i = 1;
+
+		do{
+			System.out.println("hellooo......");
+			i++;
+		}
+		while(i<=num);
+	}
+}
