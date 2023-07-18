2b2a8488fbb0e4877b6edd4ae8b8ae4a80c05622
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-18 08:22:36 +0530

    understanding the concept of function in java

---
+++ 06_functions OR methods/_01Function.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+public class _01Function{
+
+	//  defining a function.
+	public static void Hello(){
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+	}
+
+	public static void main(String args[]){
+		//  calling a function..
+		Hello();
+
+	}
+}
