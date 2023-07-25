93773ba2667c54aa83bd3753de574e3e63593ad0
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-25 15:18:02 +0530

    How to Create Arrays in java (2 Ways)

---
+++ 08_Arrays/_01arrayCreation.java
@@ -0,0 +1,18 @@
+
+
+
+public class _01arrayCreation{
+
+	public static void main(String args[]){
+
+
+
+		int marks[] = new int[50];
+
+		int numbers[] = {1, 2, 3};
+
+		String fruits[] = {"apple", "mango", "orange"};
+
+
+	}
+}
