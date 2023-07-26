da8fe2230832f5be616494fc1f6effe329c8b8a6
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-26 14:19:13 +0530

    How to find the length of Array

---
+++ 08_Arrays/_03arrayLength.java
@@ -0,0 +1,24 @@
+
+
+
+//  to find the lenght of array.....
+
+
+public class _03arrayLength{
+
+	public static void main(String args[]){
+
+
+
+		int marks[] = new int[50];
+
+		int numbers[] = {1, 2, 3};
+
+		String fruits[] = {"apple", "mango", "orange"};
+
+		System.out.println("Length of marks: " + marks.length);
+		System.out.println("Length of numbers: " + numbers.length);
+		System.out.println("Length of fruits: " + fruits.length);
+
+	}
+}
